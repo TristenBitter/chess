b535e950e329d1b7e962bb841df73b1caf68de64
First Last <email>
2024-05-24 17:08:38 -0600

    Phase 3 Server set up

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,14 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+        Spark.post("/user", this::register);
+        Spark.post("/game", this::createGame);
+        Spark.post("/session", this::login);
+        Spark.get("/game", this::listGames);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/session", this::logout);
+        Spark.delete("/db", this::clearApplication);
+
         Spark.awaitInitialization();
         return Spark.port();
     }
@@ -20,7 +28,42 @@ public class Server {
         Spark.awaitStop();
     }
 
-    //new metehods to call handeelers
+    //new methods to call handlers
+
+    private Object clearApplication(Request req, Response res) {
+
+        return 0;
+    }
+    private Object register(Request req, Response res) {
+
+        return 0;
+    }
+
+    private Object createGame(Request req, Response res) {
+
+        return 0;
+    }
+
+    private Object login(Request req, Response res) {
+
+        return 0;
+    }
+
+    private Object logout(Request req, Response res) {
+
+        return 0;
+    }
+
+    private Object listGames(Request req, Response res) {
+        res.type("application/json");
+        //return new Gson().toJson(Map.of("name", names));
+        return 0;
+    }
+
+    private Object joinGame(Request req, Response res) {
+
+        return 0;
+    }
 
 
     //handlers
