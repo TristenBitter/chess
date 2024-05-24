06bfe1d15c9269ca4859b037dd602f4ed79cd147
First Last <email>
2024-05-23 19:55:28 -0600

    Phase 3 getting started

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,34 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+    //new metehods to call handeelers
+
+
+    //handlers
+
+    //gson is a json java interpreter
+
+
+
+}
+
+
