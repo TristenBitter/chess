00d0e6f0f71d5bad1020953d8fd1f2a7612005cf
First Last <email>
2024-06-04 16:21:30 -0600

    almost done fixed some names

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,29 +1,27 @@
 package server;
 
 
-import com.google.gson.Gson;
 import dataaccess.*;
 import dataaccess.sql.MySqlAuthDAO;
 import dataaccess.sql.MySqlGameDAO;
 import dataaccess.sql.MySqlUserDAO;
 import handlers.*;
-import model.ErrorMessage;
 import spark.*;
 
 public class Server {
     //private static final DatabaseManager databaseManager = new DatabaseManager();
-    private static final MySqlUserDAO  mySqlUserDAO = new MySqlUserDAO();
-    private static final MySqlAuthDAO mySqlAuthDAO = new MySqlAuthDAO();
-    private static final MySqlGameDAO mySqlGameDAO = new MySqlGameDAO();
+    private static final MySqlUserDAO MY_SQL_USER_DAO= new MySqlUserDAO();
+    private static final MySqlAuthDAO MY_SQL_AUTH_DAO= new MySqlAuthDAO();
+    private static final MySqlGameDAO MY_SQL_GAME_DAO= new MySqlGameDAO();
 
     public int run(int desiredPort) {
         //call a method to create a database
         try {
             DatabaseManager.createDatabase();
             //call a method to create the tables for all 3 DAO's
-            mySqlUserDAO.createUserDBTable();
-            mySqlAuthDAO.createAuthDBTable();
-            mySqlGameDAO.createGameDBTable();
+            MY_SQL_USER_DAO.createUserDBTable();
+            MY_SQL_AUTH_DAO.createAuthDBTable();
+            MY_SQL_GAME_DAO.createGameDBTable();
 
         }catch(DataAccessException e){
             System.out.println("error creating database and tables");
@@ -41,18 +39,6 @@ public class Server {
         Spark.put("/game", new JoinGame());
         Spark.delete("/session", new Logout());
         Spark.delete("/db", new ClearApp());
-
-//        Spark.exception(AlreadyTakenException.class, (AlreadyTaken, request, response) ->
-//        {response.status(403);
-//            response.body(new Gson().toJson(new ErrorMessage("Error: already exists")))};
-//
-//        Spark.exception(UnauthorizedException.class, (Unauthorized, request, response) ->
-//        {response.status(401);
-//            response.body(new Gson().toJson(new ErrorMessage("Error: unauthorized")))};
-//
-//        Spark.exception(BadRequestException.class, (BadRequest, request, response) ->
-//        {response.status(400);
-//            response.body(new Gson().toJson(new ErrorMessage("Error: bad request")))};
 
 
         Spark.awaitInitialization();
