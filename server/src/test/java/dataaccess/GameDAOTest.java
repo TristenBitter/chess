5ae4b65f29618b72e2c9e0c19db1fa792d711a8a
First Last <email>
2024-06-04 23:00:51 -0600

    ALL DONE with phase 4

--- server/src/test/java/dataaccess/GameDAOTest.java
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -64,4 +64,5 @@ public class GameDAOTest {
     assertNotEquals(200, result);
 
   }
+
 }
--- server/src/test/java/dataaccess/UserDAOTest.java
+++ server/src/test/java/dataaccess/UserDAOTest.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import chess.ChessGame;
 import com.google.gson.Gson;
+import dataaccess.sql.MySqlAuthDAO;
 import dataaccess.sql.MySqlUserDAO;
 import model.*;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,11 @@ import service.RegisterService;
 import static org.junit.jupiter.api.Assertions.*;
 public class UserDAOTest {
   private final MySqlUserDAO userDAO= new MySqlUserDAO();
+  private final MySqlAuthDAO authDAO = new MySqlAuthDAO();
   private final LoginRequest loginCredentials = new LoginRequest("TristenBitter", "Tee123");
   private final LoginRequest wrongUserCredentials = new LoginRequest("kyle", "hello");
   private final RegisterRequest userData = new RegisterRequest("TristenBitter", "Tee123", "tristenkbitter@gmail.com");
+  private final UserData gameData = new UserData("kylee", "koolAid", "littleRascal23@gmail.com");
   @Test
   public void userSuccess() throws Exception{
 
@@ -44,5 +47,55 @@ public class UserDAOTest {
     LoginService login = new LoginService(wrongUserCredentials);
     AuthData result = login.loginUser(wrongUserCredentials);
     assertEquals(null, result);
+  }
+
+  @Test
+  public void insertUserDataSuccess() throws Exception{
+
+    DatabaseManager.createDatabase();
+    userDAO.createUserDBTable();
+
+    ClearService clearDB = new ClearService();
+    clearDB.clearDB();
+
+    userDAO.insertUserData(gameData);
+    assertTrue(userDAO.verifyUser("kylee", "koolAid"));
+  }
+
+  @Test
+  public void insertUserDataFailure() throws Exception{
+
+    DatabaseManager.createDatabase();
+    userDAO.createUserDBTable();
+
+    ClearService clearDB = new ClearService();
+    clearDB.clearDB();
+
+    userDAO.insertUserData(gameData);
+    assertFalse(userDAO.verifyUser("dude", "hola"));
+  }
+
+  @Test
+  public void clearUserSuccess() throws Exception{
+
+    DatabaseManager.createDatabase();
+    userDAO.createUserDBTable();
+    authDAO.createAuthDBTable();
+    ClearService clearDB = new ClearService();
+    clearDB.clearDB();
+    assertFalse(authDAO.validateAuthToken("tee"));
+  }
+
+  @Test
+  public void clearUserFailure() throws Exception{
+
+    DatabaseManager.createDatabase();
+    userDAO.createUserDBTable();
+    authDAO.createAuthDBTable();
+    ClearService clearDB = new ClearService();
+    clearDB.clearDB();
+    userDAO.insertUserData(new UserData("sea", "t", "gameID" ));
+    AuthData data = authDAO.makeAuthToken("pee");
+    assertTrue(authDAO.validateAuthToken(data.authToken()));
   }
 }
