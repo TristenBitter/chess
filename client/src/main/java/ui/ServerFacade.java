243d5fde9db64a972f43b378335b12c6e76af496
First Last <email>
2024-06-07 14:45:35 -0600

    ui

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,6 +1,21 @@
 package ui;
 
+import java.util.Scanner;
 public class ServerFacade {
+    public static void main(String[] args) throws Exception {
+      while (true) {
+        System.out.printf("Type your numbers%n>>> ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        var numbers = line.split(" ");
 
+        int result = 0;
+        for (var number : numbers) {
+          result += Integer.parseInt(number);
+        }
+        var equation = String.join(" + ", numbers);
+        System.out.printf("%s = %d%n", equation, result);
+      }
+    }
 
 }
